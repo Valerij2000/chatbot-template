4fd3234875bcdbc1dc6175f5ba589455e992d402
Valerij <valery.shumkov@mail.ru>
2021-11-14 23:02:29 +0300

    create full chat bot edit

--- chatbot/chatbot.config.js
+++ chatbot/chatbot.config.js
@@ -20,17 +20,6 @@ if (!fingerprint) {
     localStorage.setItem(configChatbot.key, fingerprint)
   });
 }
-// при клике по кнопке configChatbot.btn
-document.querySelector(configChatbot.btn).onclick = function (e) {
-  this.classList.add('d-none');
-  const $tooltip = this.querySelector('.chatbot__tooltip');
-  if ($tooltip) {
-    $tooltip.classList.add('d-none');
-  }
-  configChatbot.root.classList.toggle('chatbot_hidden');
-  if (chatbot) {
-    return;
-  }
   // получение json-файла, содержащего сценарий диалога для чат-бота через AJAX
   const request = new XMLHttpRequest();
   request.open('GET', configChatbot.replicas, true);
@@ -52,33 +41,3 @@ document.querySelector(configChatbot.btn).onclick = function (e) {
     }
   };
   request.send();
-};
-
-// подсказка для кнопки
-const $btn = document.querySelector(configChatbot.btn);
-$btn.addEventListener('mouseover', function (e) {
-  const $tooltip = $btn.querySelector('.chatbot__tooltip');
-  if (!$tooltip.classList.contains('chatbot__tooltip_show')) {
-    $tooltip.classList.remove('d-none');
-    setTimeout(function () {
-      $tooltip.classList.add('chatbot__tooltip_show');
-    }, 0);
-  }
-});
-$btn.addEventListener('mouseout', function (e) {
-  const $tooltip = $btn.querySelector('.chatbot__tooltip');
-  if ($tooltip.classList.contains('chatbot__tooltip_show')) {
-    $tooltip.classList.remove('chatbot__tooltip_show');
-    setTimeout(function () {
-      $tooltip.classList.add('d-none');
-    }, 200);
-  }
-});
-
-setTimeout(function () {
-  const tooltip = document.querySelector('.chatbot__tooltip');
-  tooltip.classList.add('chatbot__tooltip_show');
-  setTimeout(function () {
-    tooltip.classList.remove('chatbot__tooltip_show');
-  }, 10000)
-}, 10000);
